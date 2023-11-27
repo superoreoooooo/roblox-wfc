c9a5d87ecd82707a10a8ae3689d34b67f9a102cf
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-11-27 23:44:45 +0900

    update + fix algorithm

--- wfc.lua
+++ wfc.lua
@@ -1,27 +1,103 @@
 local cellDatas = {
-	type1 = {up = "1A", down = "2A", left = "3A", right = "4F"},
-	type2 = {up = "4F", down = "1A", left = "2A", right = "3A"},
-	type3 = {up = "3A", down = "4F", left = "1A", right = "2A"},
-	type4 = {up = "2A", down = "3A", left = "4F", right = "1A"}
+	type1 = {up = "BCB", down = "BAB", left = "BCB", right = "BAB", rev = 0, origin = "type1"},
+	type2 = {up = "BBB", down = "BBB", left = "BAB", right = "BAB", rev = 0, origin = "type2"},
+	type3 = {up = "BBB", down = "BAB", left = "BAB", right = "BAB", rev = 0, origin = "type3"},
+	type4 = {up = "BCB", down = "BAB", left = "BAB", right = "BAB", rev = 0, origin = "type4"},
+	type5 = {up = "BBB", down = "BAB", left = "BBB", right = "BAB", rev = 0, origin = "type5"}
 }
 
 local cellTypes = {}
 
-local gridSize = 8
+local gridSize = 10
 
 local tryCount = 0
 
+local CELL_SIZE = Vector3.new(64, 17, 64)
+
 grid = {}
 
-local function init()
+local function getCellData(cellType)
+	return cellDatas[cellType]
+end
+
+local function rotate(cellData, deg90, cellType)
+	local newCell = {}
+	if (deg90 == 1) then
+		newCell["up"] = cellData.left
+		newCell["right"] = cellData.up
+		newCell["down"] = cellData.right
+		newCell["left"] = cellData.down
+	elseif (deg90 == 2) then
+		newCell["up"] = cellData.down
+		newCell["right"] = cellData.left
+		newCell["down"] = cellData.up
+		newCell["left"] = cellData.right
+	elseif (deg90 == 3) then
+		newCell["up"] = cellData.right
+		newCell["right"] = cellData.down
+		newCell["down"] = cellData.left
+		newCell["left"] = cellData.up
+	end
+	newCell["rev"] = deg90
+	newCell["origin"] = cellType
+	return newCell
+end
+
+local function setData()
+	local newCellDatas = {}
 	cellTypes = {}
-	grid = {}
-	tryCount = 0
 	
+	local cnt = 0
+	for _, _ in pairs(cellDatas) do
+		cnt += 1
+	end
+	
+	print(cnt)
+	
+	local typeC = cnt + 1
+	
+	for deg = 1, 3 do
+		for typeS = 1, cnt do
+			local ocn = "type" .. typeS
+			local originCell = getCellData(ocn)
+			newCellDatas["type" .. typeC] = rotate(originCell, deg, ocn)
+			typeC += 1
+		end
+	end
+	
+	--for x = 1, 4 do
+	--	for y = 1, 3 do
+	--		local ocn = "type" .. x
+	--		local originCell = getCellData(ocn)
+	--		newCellDatas["type" .. typeC] = rotate(originCell, y, ocn)
+	--		typeC += 1
+	--	end
+	--end
+
+	--for k, v in pairs(cellDatas) do
+	--	for x = 1, 3 do
+	--		newCellDatas["type" .. typeC] = rotate(v, x, k)
+	--		typeC += 1
+	--	end
+	--end
+
+	--print(newCellDatas)
+
+	for k, v in pairs(newCellDatas) do
+		cellDatas[k] = v
+	end
+
 	for k, _ in pairs(cellDatas) do
 		table.insert(cellTypes, k)
 	end
-	
+
+	print(cellDatas, cellTypes)
+end
+
+local function init()
+	grid = {}
+	tryCount = 0
+
 	for z = 1, gridSize do
 		grid[z] = {}
 		for x = 1, gridSize do
@@ -35,19 +111,19 @@ end
 
 local function getEntropy(z, x)
 	local cell = grid[z][x]
-	
+
 	return #cell.possibleTypes
 end
 
 local function observe()
 	local lowestEntropy = #cellTypes
 	local collapsableCells = {}
-	
+
 	for z = 1, gridSize do
 		for x = 1, gridSize do
 			if (lowestEntropy >= getEntropy(z, x)) then
 				local cell = grid[z][x]
-				
+
 				if (cell.collapsed == false) then
 					if (lowestEntropy ~= getEntropy(z, x)) then
 						collapsableCells = {}
@@ -60,10 +136,10 @@ local function observe()
 
 	if (tryCount == 0) then
 		collapsableCells = {}
-		table.insert(collapsableCells, {z = 1, x = 1})
+		table.insert(collapsableCells, {z = math.random(2, gridSize - 1), x = math.random(2, gridSize - 1)})
 		tryCount += 1
 	end
-	
+
 	return collapsableCells
 end
 
@@ -72,24 +148,20 @@ local function collapse(lowEntropyCells)
 	local z = pick.z
 	local x = pick.x
 	local cell = grid[z][x]
-	
+
 	if (cell.collapsed == false) then
 		local typePick = math.random(#cell.possibleTypes)
 		local t = cell.possibleTypes[typePick]
-		
+
 		cell.possibleTypes = {t}
 		cell.collapsed = true
-		
-		print(z, x, cell, " collapsed!")
-		
+
+		--print(z, x, cell, " collapsed!")
+
 		return z, x
 	end
-	
-	return nil
-end
 
-local function getCellData(cellType)
-	return cellDatas[cellType]
+	return nil
 end
 
 local function isPossibleSide(cell1Type, cell2Type, cell1Side)
@@ -99,7 +171,7 @@ end
 
 local function propagate(z, x)
 	local collapsedCell = grid[z][x]
-	
+
 	if (z - 1 > 0) then
 		local effectedCell = grid[z - 1][x]
 		local possibleTypes = {}
@@ -115,7 +187,7 @@ local function propagate(z, x)
 			return false
 		end
 	end
-	
+
 	if (z + 1 < gridSize) then
 		local effectedCell = grid[z + 1][x]
 		local possibleTypes = {}
@@ -131,7 +203,7 @@ local function propagate(z, x)
 			return false
 		end
 	end
-	
+
 	if (x - 1 > 0) then
 		local effectedCell = grid[z][x - 1]
 		local possibleTypes = {}
@@ -163,8 +235,8 @@ local function propagate(z, x)
 			return false
 		end
 	end
-	
-	print(grid)
+
+	--print(grid)
 	return true
 end
 
@@ -173,58 +245,152 @@ local function performWFC()
 
 	local cz, cx = collapse(l)
 
-	print(cz, cx, l)
-	
+	--print(cz, cx, l)
+
 	local t = propagate(cz, cx)
-	
+
 	if (not t) then
-		print("error! regenerate!")
+		--print("error! regenerate!")
 		init()
 	end
-	
+
 	return t
 end
 
-local function getOutput()
-	local CELL_SIZE = Vector3.new(40, 10, 40)
+local function getRealType(cellType)
+	return cellDatas[cellType]
+end
 
+local ceiling = game.ReplicatedStorage.celling
+
+local function getOutput(level)
+	local output = {}
+	local possibleSpawnPoints = {}
+	local possibleExitPoints = {}
 	if grid then
 		for z = 1, gridSize do
 			for x = 1, gridSize do
 				if (grid[z][x].collapsed) then
 					local cellType = grid[z][x].possibleTypes[1]
-					--print(getCellType(cellType))
-					if cellType then
+					local realType = getRealType(cellType).origin
+					local rev = getRealType(cellType).rev
+					local pattern = "(%a+)(%d+)"
+					
+					local randNum = math.random(0, 3)
+					local _, number = realType:match(pattern)
+					local randType = "type" .. (randNum * 5) + number
+					
+					--print(realType, randType, rev, randNum)
+					
+					print(z, x, randType, rev)
+					
+					if randType then
 						--print(string.format("Cell (%d, %d) Type: %s", z, x, cellType))
-						if game.Workspace.Cells:FindFirstChild(cellType) then
+						if game.Workspace.Cell:FindFirstChild(randType) then
+							local origin = game.Workspace.Cell:WaitForChild(randType)
+							local copy = origin:Clone()
+							local newPosition = Vector3.new(
+								(x - 1) * CELL_SIZE.X + (CELL_SIZE.X / 2),
+								150,
+								(z - 1) * CELL_SIZE.Z + (CELL_SIZE.Z / 2) + (level - 1) * 100
+							)
+							local rotation = CFrame.Angles(0, math.rad(-90 * rev), 0)
+							copy:PivotTo(CFrame.new(newPosition) * rotation)
+							if (randType == "type10" or randType == "type20") then
+								copy:PivotTo(copy:GetPivot() * CFrame.Angles(0, math.rad(90), math.rad(-90)))
+								print("pivoting!")
+							end
+							copy.Parent = workspace
 							
-							local cellTemplate = game.Workspace.Cells:WaitForChild(cellType)
-							local newCell = cellTemplate:Clone()
-
-							newCell.Position = Vector3.new((x - 1) * CELL_SIZE.X, 6.5, (z - 1) * CELL_SIZE.Z)
-							newCell.Parent = workspace  -- You can set a different parent if needed
-
-							newCell.Anchored = true  -- Ensure the part is anchored
-							newCell.CanCollide = true  -- If you want the part to be solid
+							local pos = Vector3.new(
+								(x - 1) * CELL_SIZE.X + (CELL_SIZE.X / 2), 
+								172.8, 
+								(z - 1) * CELL_SIZE.Z + (CELL_SIZE.Z / 2) + (level - 1) * 100
+							)
+							local cp = ceiling:Clone()
+							cp:PivotTo(CFrame.new(pos) * CFrame.Angles(0, math.rad(-90 * rev), 0))
+							cp.Parent = workspace
+						end
+						if (output[cellType] == nil) then
+							output[cellType] = 1
+						else
+							output[cellType] += 1
+						end
+						if (randType == "type1" or randType == "type3" or randType == "type14") then
+							local obj = {z = z, x = x}
+							table.insert(possibleSpawnPoints, obj)
 						end
 					end
 				end
 			end
 		end
+		print(output)
+		
+		local sl = workspace.SpawnLocation
+		local rand = math.random(#possibleSpawnPoints)
+		local obj = possibleSpawnPoints[rand]
+		sl.Position = Vector3.new(
+			(obj.x - 1) * CELL_SIZE.X + (CELL_SIZE.X / 2), 
+			157, 
+			(obj.z - 1) * CELL_SIZE.Z + (CELL_SIZE.Z / 2) + (level - 1) * 100
+		)
+		sl.Parent = workspace
+		
+		local p1 = Instance.new("Part")
+		p1.Size = Vector3.new(CELL_SIZE.X * gridSize, 46, 2)
+		p1.Position = Vector3.new(
+			(CELL_SIZE.X * gridSize) / 2, 
+			150, 
+			-1 + (level - 1) * 100
+		)
+		p1.Anchored = true
+		p1.Material = Enum.Material.Concrete
+		p1.Parent = workspace
+
+		local p2 = Instance.new("Part")
+		p2.Size = Vector3.new(2, 46, CELL_SIZE.X * gridSize)
+		p2.Position = Vector3.new(
+			-1, 
+			150, 
+			(CELL_SIZE.X * gridSize) / 2 + (level - 1) * 100
+		)
+		p2.Anchored = true
+		p2.Material = Enum.Material.Concrete
+		p2.Parent = workspace
+
+		local p3 = Instance.new("Part")
+		p3.Size = Vector3.new(CELL_SIZE.X * gridSize, 46, 2)
+		p3.Position = Vector3.new(
+			(CELL_SIZE.X * gridSize) / 2, 
+			150, 
+			(CELL_SIZE.X * gridSize) + 1 + (level - 1) * 100
+		)
+		p3.Anchored = true
+		p3.Material = Enum.Material.Concrete
+		p3.Parent = workspace
+
+		local p4 = Instance.new("Part")
+		p4.Size = Vector3.new(2, 46, CELL_SIZE.X * gridSize)
+		p4.Position = Vector3.new(
+			(CELL_SIZE.X * gridSize) + 1, 
+			150, 
+			(CELL_SIZE.X * gridSize) / 2 + (level - 1) * 100
+		)
+		p4.Anchored = true
+		p4.Material = Enum.Material.Concrete
+		p4.Parent = workspace
+		
 	else
 		print("Failed to generate a valid grid after multiple attempts.")
 	end
-end
+end 
 
-
-
-
-local function run()
+local function run(level)
 	local done = false
 	
 	while not done do
 		done = performWFC()
-		
+
 		for z = 1, gridSize do
 			for x = 1, gridSize do
 				if (grid[z][x].collapsed == false) then
@@ -236,11 +402,11 @@ local function run()
 
 	print(grid)
 
-	getOutput()
+	getOutput(level)
 end
 
+--setData()
 
-init()
+--init()
 
-
-run()
+--run(level)
